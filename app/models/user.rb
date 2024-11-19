3aa6b8c4109c73d3de120465273c2aae625db631
ayaka <beautifuldays057@gmail.com>
2024-11-19 19:01:24 +0900

    通知機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,6 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-
-
       user.name = auth.info.name
       # user.profile = auth.info.profile
       user.email = auth.info.email
