0378ddc77e7d90e6bff64c771a21e55edee3c200
ayaka <beautifuldays057@gmail.com>
2024-11-22 14:39:16 +0900

    お問い合わせフォーム実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'contacts/new'
+  get 'contacts/create'
   get 'terms', to: 'pages#terms'
   get 'privacy_policy', to: 'pages#privacy_policy'
   get 'questions/new'
@@ -28,6 +30,8 @@ Rails.application.routes.draw do
   resources :manuals, only: %i[index show], param: :category do
     post 'complete', on: :member
   end
+
+  resources :contacts, only: [:new, :create]
 
   resources :notifications, only: :index
 
